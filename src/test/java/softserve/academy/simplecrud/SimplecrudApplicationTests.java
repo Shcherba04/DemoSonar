58a922a15eb9bc2994e513533e51eefcc310f620
StefanBohdanShcherba <99718366+Shcherba04@users.noreply.github.com>
2025-03-23 20:42:45 +0200

    Added comment

--- src/test/java/softserve/academy/simplecrud/SimplecrudApplicationTests.java
+++ src/test/java/softserve/academy/simplecrud/SimplecrudApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SimplecrudApplicationTests {
 
+    //just testing if context loads
     @Test
     void contextLoads() {
     }
